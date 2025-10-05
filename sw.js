c91f11f83d7194b5f4a22c7383d5151ead07c80c
Mtpc9272070 <poloatenea@gmail.com>
2025-10-05 06:44:34 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js (Service Worker)
-const CACHE_NAME = 'aduweb-pwa-v7';
+const CACHE_NAME = 'aduweb-pwa-v8';
 // Lista de archivos críticos que deben guardarse para funcionar offline
 const urlsToCache = [
   '/beta/',
